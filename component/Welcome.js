d59aed62d95e97814157dfad1f9baf06f0e610a8
jennkim0210 <131511782+jennkim0210@users.noreply.github.com>
2024-05-27 13:21:13 +0900

    Update Welcome.js

--- component/Welcome.js
+++ component/Welcome.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 
 const Welcome = (props) => {
-  const {startQuestion, setStartQuestion} = props;
-  
-  const stepToQuestion = ()=>{
+  const { startQuestion, setStartQuestion } = props;
+
+  const stepToQuestion = () => {
     setStartQuestion(!startQuestion);
-  }
+  };
 
   return (
     <main className="content">
-      <h1 className="content__title">MBTI TRAVEL</h1>
-      <h2 className="content__sub-title">질문으로 여행 MBTI 를 ~~~~</h2>
-      <button className="content__link btn btn--s btn--primary" onClick={e => stepToQuestion()}>Start</button>
+      <h1 className="content__title">여행 MBTI 알아보기 🧐 </h1>
+      <h2 className="content__sub-title">딱 맞는 여행지✈️를 찾아드려요! </h2>
+      <button
+        className="content__link btn btn--s btn--primary"
+        onClick={(e) => stepToQuestion()}
+      >
+        Start
+      </button>
     </main>
   );
 };
